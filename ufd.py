41c9ca3c0ba61a0b948e723f6169686237fb45ce
Jaredmbenitez <61889494+Jaredmbenitez@users.noreply.github.com>
2020-06-12 11:27:05 -0700

    Use Css selectors instead of find() function to navigate html tags

--- ufd.py
+++ ufd.py
@@ -23,35 +23,36 @@ character_page_soup = soup(character_page_html, "html.parser")
 def printCharInfo(urlName):
 
 	Move_Container = character_page_soup.findAll("div", {"class": "movecontainer"})
-
+	
 	for move in Move_Container:
-
-		print("Move Name:" , move.find("div",{"class": "movename"}).text.strip())#Other: move.select(."movename")
+		
+		
+		print("Move Name:" , move.select(".movename")[0].text.strip())#Other: move.select(."movename")
 
 		if move.select('.startup'):
-			print("Startup Frames: ",move.find("div",{"class": "startup"}).text.strip())
+			print("Startup Frames: ",move.select(".startup")[0].text.strip())
 		if move.select('.totalframes'):
-			print("Total Frames: ",move.find("div",{"class": "totalframes"}).text.strip())
+			print("Total Frames: ",move.select(".totalframes")[0].text.strip())
 		if move.select('.activeframes'):
-			print("Active Frames: " ,move.find("div",{"class": "activeframes"}).text.strip())
+			print("Active Frames: " ,move.select(".activeframes")[0].text.strip())
 
 		if move.select('.landinglag'):
-			print("Landing Lag: ", move.find("div",{"class": "landinglag"}).text.strip())
+			print("Landing Lag: ", move.select(".landinglag")[0].text.strip())
 
 
 		if move.select('.basedamage'):
-			print("Base Damage: ", move.find("div",{"class": "basedamage"}).text.strip(), '%')
+			print("Base Damage: ", move.select(".basedamage")[0].text.strip(), '%')
 
 
 		if move.select('.shieldlag'):	# Other : if move.find('shieldstun')!= None :	
-			print("Sheild Lag: ", move.find("div",{"class": "shieldlag"}).text.strip())	
+			print("Sheild Lag: ",move.select(".shieldlag")[0].text.strip())	
 		if move.select('.shieldstun'):	
-			print("Shield Stun: ",move.find("div",{"class": "shieldstun"}).text.strip())
+			print("Shield Stun: ",move.select(".shieldstun")[0].text.strip())
 		if move.select('.advantage'):
-			print("Shield Advantage: ",move.find("div",{"class": "advantage"}).text.strip())
+			print("Shield Advantage: ",move.select(".advantage")[0].text.strip())
 		print ("-----------------------------------------------------------")
 
-
+		
 
 printCharInfo(my_url)
 #CharList = main_page_soup.findAll("div", {"class": "charactericon"})		#grabs 83 "character classes" (1 is "stats")
